fe1ba8d2392b4e0ed7455ea4c492e80f1ec2bcb6
mister-hai <81480735+mister-hai@users.noreply.github.com>
2021-06-26 13:29:36 -0500

    Update discordscraper.py

--- discordscraper.py
+++ discordscraper.py
@@ -2,7 +2,7 @@
 ################################################################################
 ## Message Scraper for discord archival                                       ##
 ################################################################################
-# Copyright (c) 2020                                     ##
+# Copyright (c) 2020                                                          ##
 #                                                                             ##
 # Permission is hereby granted, free of charge, to any person obtaining a copy##
 # of this software and associated documentation files (the "Software"),to deal##
